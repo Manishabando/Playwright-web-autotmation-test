e07122b0b6db605830ce980d0c03a5523c81f13d
Manishabando <manishabando95@gmail.com>
2026-03-18 17:00:10 +0530

    Enhance login test with logout verification
    Added logout functionality and verification to the login test.

--- login.spec.js
+++ login.spec.js
@@ -1,16 +1,22 @@
- const {test,expect} =require('@playwright/test');
+  const {test,expect} =require('@playwright/test');
 
- test("Valid test", async function({page}){
-
+   test("Valid test", async function({page})
+ {
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
-    await page.getByPlaceholder("Username").fill("Admin")
-
+    
+    await page.locator("//input[@name='Username']").fill("Admin")  
+    
     await page.getByPlaceholder("Password").fill("admin123")
 
     await page.getByRole("button", { name: "Login" }).click()
 
     await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible()
-        
- 
-})
+
+    await page.getByAltText("profile picture").click()
+
+    await page.getByText("Logout").click()
+
+    await expect(page).toHaveURL(/login/) 
+         
+}) ;
+
