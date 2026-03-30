5c96fcdcfd28f3c24ad5f702dd43e1fc3dc1d805
Manishabando <manishabando95@gmail.com>
2026-03-30 18:12:17 +0530

    Add file upload test using Playwright

---
+++ fileUploads.spec.js
@@ -0,0 +1,14 @@
+const{test,expect} =require('@playwright/test')
+const path = require('path')
+
+test("File upload test", async ({page}) => {
+  // Test implementation
+  await page.goto("https://the-internet.herokuapp.com/upload")
+  
+  const file1 = path.resolve(__dirname, '../uploads/file1.pdf');
+  await page.locator("#file-upload").setInputFiles(file1);
+
+  await page.locator("#file-submit").click()
+  expect(await page.locator("//h3")).toHaveText("File Uploaded!")
+
+});
